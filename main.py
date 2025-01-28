cfe20028d300d786f4dffa9f532462aea3962080
Pro142 <felipeemanuel20072024@gmail.com>
2025-01-27 19:34:51 -0500

    Discoooooooooooooooooooor
    Kodland M1L2

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print('hola mundo')
-nombre = input('Nombre')
-print ('hola', nombre)
+import random
+
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+otra_variable = int(input ('Cantidad de caracteres para tu contrseña'))
+vacia = ''
+for i in range(otra_variable):
+    vacia += (random.choice(caracteres))
+print (vacia)
